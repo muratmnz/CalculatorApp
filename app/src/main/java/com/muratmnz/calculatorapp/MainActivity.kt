95b994ff2ba8c013d2969c32bf60557536c69e8d
muratmnz <minazzmurat@gmail.com>
2023-02-23 23:17:50 +0300

    feature: added digits and clear function

--- app/src/main/java/com/muratmnz/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/muratmnz/calculatorapp/MainActivity.kt
@@ -2,18 +2,33 @@ package com.muratmnz.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.muratmnz.calculatorapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private var txtInput: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //view binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        txtInput = findViewById(R.id.resultText)
+
+    }
+
+    fun onDigit(view: View) {
+        txtInput?.append((view as Button).text)
+    }
+
+    fun onClear(view: View) {
+        txtInput?.text = ""
     }
 }
